daadb2eae653ba83e4947a94f3d91ac28a78874e
SOSHSU <tigerhsu@pacificamerican.org>
2021-01-26 14:42:44 +0800

    score and penalty

--- mechanic.js
+++ mechanic.js
@@ -12,6 +12,7 @@ function minusScore()
     }
     document.querySelector(".homeScore").innerHTML = score
 }
+
 var score2 = 0
 function addScore2()
 {
@@ -25,4 +26,42 @@ function minusScore2()
         score2--
     }
     document.querySelector(".guestScore").innerHTML = score2
+}
+
+var period = 1
+function nextPeriod()
+{
+    if(period >= 3)
+    {
+        period = 1
+    }
+    else
+    {
+        period++
+    }
+    document.querySelector(".periodtime").innerHTML = period
+}
+
+var name1 = ""
+function addName1()
+{
+    name1 = prompt("What is the name?")   
+    document.querySelector(".player1").innerHTML = name1
+}
+function removeName1()
+{
+    name1 = ""
+    document.querySelector(".player1").innerHTML = name1
+}
+
+var name2 = ""
+function addName2()
+{
+    name2 = prompt("What is the name?")   
+    document.querySelector(".player2").innerHTML = name2
+}
+function removeName2()
+{
+    name2 = ""
+    document.querySelector(".player2").innerHTML = name2
 }
